99e8adb97dd48b54b9169560e5952fc0daf1981d
korthj <khj12005@gmail.com>
2017-08-25 21:25:56 +0900

    17/08/25 21:25 commit

--- SingleWebProject.web/WebContent/main/mainCover.jsp
+++ SingleWebProject.web/WebContent/main/mainCover.jsp
@@ -26,7 +26,7 @@
     <script src="../resources/assets/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
+    <!--[if lt IE 9]>	
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->    
@@ -44,7 +44,7 @@
 				</h3>
 				<nav>
 					<ul class="nav masthead-nav">
-						<li class="active"><a href="#">Home</a></li>
+						<li class="active"><a href="#">Login</a></li>
 						<li><a href="#">Features</a></li>
 						<li><a href="#">Contact</a></li>
 					</ul>
---
+++ SingleWebProject.web/src/Controller/UserController.java
@@ -0,0 +1,33 @@
+package Controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/User")
+public class UserController{
+	//
+	@Autowired
+	private UserService service;
+	
+	@RequestMapping("/login.do")
+	public ModelAndView loginCheck(User user,HttpSession session){
+		ModelAndView mav = new ModelAndView();
+		User users = service.loginCheck(user);
+		if(users = null){
+			mav.addObject("message","error");
+			mav.setViewName("/login.jsp");
+			return mav;
+		}else{
+			session.setAttribute("user", users);
+			mav.setViewName("/main.jsp");
+			return mav;
+		}
+	} 
+	
+}
